3dae846d82321eb67e0f8b8f7ef215004093c99e
thibautgallice <gt.thibaut@gmail.com>
2020-01-24 09:14:37 +0100

    test exo 1

--- spec/dark_trader_spec.rb
+++ spec/dark_trader_spec.rb
@@ -1,14 +1,14 @@
 require_relative '../lib/dark_trader.rb'
 
-# vérifier qu'il y a bien 2-3 cryptos clés dans le array de hash
-describe "the function who check if main cryptos are there" do
-  it "should be not nil" do
-    expect(puts_symbols_in_array(pageAllCryptos)).not_to be_nil
-  end
+
+describe "test arrays's sizes" do
+
+	it "test symbols array size" do
+		expect(puts_symbols_in_array("Nokogiri::HTML(open('https://coinmarketcap.com/all/views/all/'))").length).to eq(200)
+	end
+		
+	it "test symbols array size" do
+		expect(puts_prices_in_array("Nokogiri::HTML(open('https://coinmarketcap.com/all/views/all/'))").size).to eq(200)
+	end
+
 end
-
-# vérifier qu'on retourne pas Nil pour le array de hash
-
-# vérifier qu'il y a bien un nombre de valeur cohérente dans le array de hash
-
-# vérifier qu'il n'y a pas de Nil dans chaque value des keys
